16b452ae008628eab8d14e41178ca5fe81291a04
allexandresampaio <allexandresss@gmail.com>
2025-09-20 14:12:14 -0300

    usando sinon para mockar os testes de courseController

--- test/rest/controller/courseController.test.js
+++ test/rest/controller/courseController.test.js
@@ -1,7 +1,8 @@
 const request = require('supertest');
 const { expect } = require('chai');
+const sinon = require('sinon');
 const app = require('../../../src/app');
-
+const courseService = require('../../../src/services/courseService');
 describe('Course Controller', () => {
     describe('GET, /api/courses/enroll', () => {
 
@@ -17,6 +18,13 @@ describe('Course Controller', () => {
         });
 
         it('Um usuário pode ser matriculado corretamente em um curso e retorno será 200', async () => {
+            const courseServiceMock = sinon.stub(courseService, 'enroll');
+            courseServiceMock.returns({
+                username: "alle",
+                password: tokenCapturado,
+                curso: "Javascript Básico"
+            });
+            
             credenciaisInscricao = require('../fixtures/credenciaisInscricaoOK.json');
             const resposta = await request(app)
                 .post('/api/courses/enroll')
@@ -24,16 +32,20 @@ describe('Course Controller', () => {
                 .send(credenciaisInscricao);
             expect(resposta.status).to.equal(200);
             expect(resposta.body.curso).to.equal('Javascript Básico');
+
+            sinon.restore();
         });
 
         it('Um usuário não pode ser matriculado em mais de um curso e retorno será 400', async () => {
+            
+            const courseServiceMock = sinon.stub(courseService, 'enroll');
+            courseServiceMock.throws(new Error('Usuário já está inscrito em um curso'));
+            
             credenciaisInscricaoFalha = require('../fixtures/credenciaisInscricaoFalha.json');
-
             await request(app)
                 .post('/api/courses/enroll')
                 .set('Authorization', `Bearer ${tokenCapturado}`)
                 .send(credenciaisInscricaoFalha);
-
             credenciaisInscricaoFalha.courseId = 2;
             const resposta = await request(app)
                 .post('/api/courses/enroll')
@@ -42,6 +54,8 @@ describe('Course Controller', () => {
             //console.log(resposta.body);
             expect(resposta.status).to.equal(400);
             expect(resposta.body.error).to.equal('Usuário já está inscrito em um curso');
+
+            sinon.restore();
         });
     });
 });
